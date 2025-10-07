ff13fc1ca36cabc90572413bb5224315a7ffbf54
mikolajzielinski <mikzielinski@gmail.com>
2025-10-08 00:01:35 +0200

    Fix CORS Cache-Control header

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -278,7 +278,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
   })
 );
 app.use(morgan("dev"));
